6f5c33f2f16f176c58a5c4d640475864885239d2
joorge20 <65991700+joorge20@users.noreply.github.com>
2024-10-22 15:01:48 -0600

    Add files via upload

--- Limpieza Parte 1.sql
+++ Limpieza Parte 1.sql
@@ -1,6 +1,6 @@
 
 Select * from Catalogo_Animes;
-Select * from Visualizaciones_Anime;
+Select * from Visualizaciones;
 
 -- Se crea una tabla extra con la informacion que coincida de ambas tablas,
 -- para traer la informacion de las Visualizaciones a la tabla de Animes
@@ -12,98 +12,121 @@ se cambia el tipo de dato
 ALTER TABLE Catalogo_Animes ALTER COLUMN ANIME_ID INT;
 ALTER TABLE Visualizaciones ALTER COLUMN ANIME_ID INT;
 
-
 -- Validar si hay un ID repetido tabla Anime
-WITH ID_REPETIDOS AS (
-Select anime_id, count(anime_id) AS CANTIDAD from Catalogo_Animes
-group by anime_id)
-SELECT * FROM ID_REPETIDOS WHERE CANTIDAD >1;
+WITH id_repetidos
+     AS (SELECT anime_id,
+                Count(anime_id) AS CANTIDAD
+         FROM   catalogo_animes
+         GROUP  BY anime_id)
+SELECT *
+FROM   id_repetidos
+WHERE  cantidad > 1;
 
 -- Validar si hay un ID repetido tabla Visualziacion
-WITH ID_REPETIDOS AS (
-Select anime_id, count(anime_id) AS CANTIDAD from Visualizaciones
-group by anime_id)
-SELECT * FROM ID_REPETIDOS WHERE CANTIDAD >1;
+WITH id_repetidos
+     AS (SELECT anime_id,
+                Count(anime_id) AS CANTIDAD
+         FROM   visualizaciones
+         GROUP  BY anime_id)
+SELECT *
+FROM   id_repetidos
+WHERE  cantidad > 1; 
 
 --Validar Cruce de Tablas
-SELECT A.*,WATCHING, COMPLETED, ON_HOLD, DROPPED FROM
-Catalogo_Animes a LEFT JOIN Visualizaciones b ON a.anime_id = b.anime_id
-;
+SELECT A.*,
+       watching,
+       completed,
+       on_hold,
+       dropped
+FROM   catalogo_animes a
+       LEFT JOIN visualizaciones b
+              ON a.anime_id = b.anime_id; 
 
 -- Las columnas tienen un valor denomindado UNKNOWN en ciertas columnas, pasare ese valor
 -- a NULL en las columnas que sean Numericas, para pasarlas en limpio a la siguiente tabla a crear
-UPDATE Catalogo_Animes
-SET (
-SCORE = NULL,
-Episodes = NULL,
-RANK = NULL,
-Popularity = NULL,
-FAVORITES = NULL,
-Scored_By = NULL,
-Members = NULL
-Select * from animes WHERE (Episodes = 'UNKNOWN' OR
-SCORE = 'UNKNOWN' OR
-ranking = 'UNKNOWN' OR
-Popularity = 'UNKNOWN' OR
-FAVORITES = 'UNKNOWN' OR
-Scored_By = 'UNKNOWN' OR
-Members = 'UNKNOWN')
-
--- Valdiar tabla Visualizaciones
-Select * from visualizaciones WHERE (
-Watching = 'UNKNOWN' OR
-Completed = 'UNKNOWN' OR
-On_Hold = 'UNKNOWN' OR
-Dropped = 'UNKNOWN')
-
+UPDATE Catalogo_Animes set SCORE = 1 where SCORE = 'UNKNOWN';
+UPDATE Catalogo_Animes set Episodes = 0 where Episodes = 'UNKNOWN';
+UPDATE Catalogo_Animes set RANK = 0 where RANK = 'UNKNOWN';
+UPDATE Catalogo_Animes set Popularity = 0 where Popularity = 'UNKNOWN';
+UPDATE Catalogo_Animes set FAVORITES = 0 where FAVORITES = 'UNKNOWN';
+UPDATE Catalogo_Animes set Scored_By = 0 where Scored_By = 'UNKNOWN';
+UPDATE Catalogo_Animes set Members = 0 where Members = 'UNKNOWN';
 
 --Validar Largo Columnas para cambiar a NVARCHAR
-WITH LARGO AS
-(SELECT max(LEN(NAME)) Name, max(LEN(English_name)) English_name,max(LEN(Genres)) Genres,
-max(LEN(Episodes)) Episodes,max(LEN(Producers)) Producers,
-max(LEN(Licensors)) Licensors, max(LEN(Studios)) Studios, max(LEN(Source)) Source, 
-max(LEN(Rating)) Rating, max(LEN(image_url)) image_url
-FROM Catalogo_Animes)
-SELECT * from Largo;
+SELECT Max(Len(NAME))         NAME,
+       Max(Len(english_name)) English_name,
+       Max(Len(genres))       Genres,
+       Max(Len(episodes))     Episodes,
+       Max(Len(producers))    Producers,
+       Max(Len(licensors))    Licensors,
+       Max(Len(studios))      Studios,
+       Max(Len(source))       Source,
+       Max(Len(rating))       Rating,
+       Max(Len(image_url))    image_url
+FROM   catalogo_animes; 
 
 Select * from Catalogo_Animes;
 
-CREATE TABLE Animes
-(
-	anime_id	INT,
-	NAME	VARCHAR(150),
-	SCORE	FLOAT,
-	GENERO  NVARCHAR(100),
-	TYPE	NVARCHAR(20),
-	EPISODES	INT,
-	Fecha_Estreno nvarchar(50),
-	Temporada nvarchar(50),
-	status nvarchar(20),
-	producers nvarchar(400),
-	licensors nvarchar(100),
-	Studio nvarchar(150),
-	Source nvarchar(200),
-	duration nvarchar(50),
-	raiting nvarchar(50),
-	ranking int,
-	popularity int,
-	favorites int,
-	scored_by int,
-	members int,
-	watching int,
-	Completed int,
-	On_Hold INT,
-	Dropped int
-);
+CREATE TABLE animes
+  (
+     anime_id      INT,
+     NAME          VARCHAR(150),
+     score         FLOAT,
+     genero        NVARCHAR(100),
+     type          NVARCHAR(20),
+     episodes      INT,
+     fecha_estreno NVARCHAR(50),
+     temporada     NVARCHAR(50),
+     status        NVARCHAR(20),
+     producers     NVARCHAR(400),
+     licensors     NVARCHAR(100),
+     studio        NVARCHAR(150),
+     source        NVARCHAR(200),
+     duration      NVARCHAR(50),
+     raiting       NVARCHAR(50),
+     ranking       INT,
+     popularity    INT,
+     favorites     INT,
+     scored_by     INT,
+     members       INT,
+     watching      INT,
+     completed     INT,
+     on_hold       INT,
+     dropped       INT
+  ); 
 
 Select * from Catalogo_animes;
 Select * from animes;
 Select * from Visualizaciones;
--- Insert tabla Nueva
-Insert into Animes 
-SELECT a.anime_id,a.name, cast(score as float), Genres,Type,Episodes,Aired,Premiered,Status,Producers,Licensors,Studios,
-Source,Duration,Rating,Rank,Popularity,Favorites,Scored_By,Members
-,WATCHING, COMPLETED, ON_HOLD, DROPPED FROM
-Catalogo_Animes a LEFT JOIN Visualizaciones b ON a.anime_id = b.anime_id;
+
+-- Insert Datos en la tabla Nueva
+INSERT INTO animes
+SELECT a.anime_id,
+       a.NAME,
+       Cast(score AS FLOAT),
+       genres,
+       type,
+       episodes,
+       aired,
+       premiered,
+       status,
+       producers,
+       licensors,
+       studios,
+       source,
+       duration,
+       rating,
+       rank,
+       popularity,
+       favorites,
+       scored_by,
+       members,
+       watching,
+       completed,
+       on_hold,
+       dropped
+FROM   catalogo_animes a
+       LEFT JOIN visualizaciones b
+              ON a.anime_id = b.anime_id; 
 
 Select * from animes;
